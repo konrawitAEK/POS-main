32781d911f01e1d8d850309a6e4f7b958f85efd9
konrawitAEK <aekpeerat2542@gmail.com>
2026-05-31 23:07:25 +0700

    [frontend] validate token go back login

--- front-end/src/components/MainLayout.tsx
+++ front-end/src/components/MainLayout.tsx
@@ -1,5 +1,19 @@
+'use client'
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+import { useAuthStore } from '@/store/authStore'
 import Sidebar from './Sidebar'
+
 export default function MainLayout({ children }: { children: React.ReactNode }) {
+  const router = useRouter()
+  const token  = useAuthStore((s) => s.token)
+
+  useEffect(() => {
+    if (!token) router.replace('/login')
+  }, [token, router])
+
+  if (!token) return null
+
   return (
     <div className="flex min-h-screen bg-gray-50">
       <Sidebar />
